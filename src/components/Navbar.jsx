87fdc7c27df9125107cfecaafe302e88be4611f1
mcelant <massimiliano.celant@freeman.com>
2022-10-23 15:10:46 -0500

    v0.0.1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="container w-screen mx-auto min-w-screen bg-white">
+        <div className="p-5">
+            <div className="flex justify-start">
+                Estephania Santos
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
